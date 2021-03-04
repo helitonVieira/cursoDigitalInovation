bf84fd32b97e32083296ddd65ae7cad23a53675e
heliton <helitondba@gmail.com>
2021-03-04 09:27:42 -0300

    criando uma inner class endereco dento class pessoa

--- src/main/kotlin/one/curso/kotlin/Pessoa.kt
+++ src/main/kotlin/one/curso/kotlin/Pessoa.kt
@@ -3,9 +3,14 @@ package one.curso.kotlin
 class Pessoa {
     val nome: String = "Heliton Silva Vieira"
     val cpf: String = "064.847.146.33"
+
+    inner class Endereco{
+        var rua: String = "Rua Teste"
+    }
 }
 fun main() {
     val pessoa = Pessoa()
     println(pessoa.nome)
     println(pessoa.cpf)
+    println(pessoa.Endereco().rua)
 }
